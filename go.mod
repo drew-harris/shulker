250b4c2f985afbdd90c89aa36840b23eac2fcd78
Drew Harris <harrisd@smu.edu>
2023-08-10 01:24:22 -0500

    Flags + version support

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/integrii/flaggy v1.5.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
--- main.go
+++ main.go
@@ -9,30 +9,52 @@ import (
 	"github.com/drewharris/shulker/engine"
 	"github.com/drewharris/shulker/model"
 	"github.com/drewharris/shulker/styles"
+	"github.com/integrii/flaggy"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
+var version string = "unversioned" // Replace with ldflags
+
 func main() {
-	os.Setenv("DOCKER_API_VERSION", "1.41")
+	flaggy.SetName("Shulker")
+	flaggy.SetVersion(version)
+	flaggy.SetDescription("A tui (terminal user interface) for efficient plugin development at the HuMIn Game Labs")
 
-	d, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		fmt.Println(styles.Error.Render("Error connecting to docker " + err.Error()))
-		os.Exit(1)
+	var noDocker bool
+
+	flaggy.Bool(&noDocker, "n", "no-docker", "Use the host system instead of docker")
+
+	flaggy.Parse()
+
+	var program *tea.Program
+
+	if noDocker {
+		fmt.Println("Not using docker")
+		return
+	} else {
+		os.Setenv("DOCKER_API_VERSION", "1.41")
+
+		d, err := client.NewClientWithOpts(client.FromEnv)
+		if err != nil {
+			fmt.Println(styles.Error.Render("Error connecting to docker " + err.Error()))
+			os.Exit(1)
+		}
+
+		// Test connection by getting containers
+		_, err = d.ImageList(context.Background(), types.ImageListOptions{All: true})
+		if err != nil {
+			fmt.Println(styles.Error.Render("\n   Can't connect to Docker, Is it running?"))
+			os.Exit(1)
+		}
+
+		dEngine := engine.NewDockerEngine(d)
+
+		program = tea.NewProgram(model.InitialModel(dEngine), tea.WithAltScreen())
 	}
 
-	// Test connection by getting containers
-	_, err = d.ImageList(context.Background(), types.ImageListOptions{All: true})
-	if err != nil {
-		fmt.Println(styles.Error.Render("\n   Can't connect to Docker, Is it running?"))
-		os.Exit(1)
-	}
-
-	dEngine := engine.NewDockerEngine(d)
-
-	p := tea.NewProgram(model.InitialModel(dEngine), tea.WithAltScreen())
+	// Docker
 
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
@@ -41,7 +63,7 @@ func main() {
 	}
 
 	defer f.Close()
-	if _, err := p.Run(); err != nil {
+	if _, err := program.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
