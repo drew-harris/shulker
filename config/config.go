f9492d67ada125b510a961c9eaab12d1316919e4
Drew Harris <harrisd@smu.edu>
2023-08-10 21:32:44 -0500

    Barely done host engine

---
+++ config/config.go
@@ -0,0 +1,3 @@
+package config
+
+var ShulkerboxUrl = "https://minecraft-hgl-drew.s3.amazonaws.com/shulkerbox.zip"
---
+++ engine/hostengine.go
@@ -0,0 +1,49 @@
+package engine
+
+import (
+	"errors"
+	"net/http"
+	"os"
+
+	"github.com/drewharris/shulker/config"
+	"github.com/drewharris/shulker/types"
+)
+
+type HostEngine struct {
+}
+
+func DownloadShulkerbox(pwd string) error {
+	out, err := os.Create(pwd + "/shulker_data.zip")
+	defer out.Close()
+
+	resp, err := http.Get(config.ShulkerboxUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func (h *HostEngine) EnsureSetup(sub chan types.OutputMsg) error {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	_, err = os.Stat(pwd + "/shulker_data/spigot")
+	if errors.Is(err, os.ErrNotExist) {
+		// Download file
+		DownloadShulkerbox(pwd)
+	} else if err != nil {
+		return err // Worst case option
+	}
+
+	return nil
+}
+
+func (h *HostEngine) StartServer(sub chan types.OutputMsg) error       { return nil }
+func (h *HostEngine) RebuildAllPlugins(sub chan types.OutputMsg) error { return nil }
+func (h *HostEngine) Shutdown() error                                  { return nil }
+
+// SendCommandToSpigot(cmd string) error
+func (h *HostEngine) CanAttach() bool { return false }
